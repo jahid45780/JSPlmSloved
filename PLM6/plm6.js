56ddab98dd2f65ce71472e15b05b64c5fcd58c0b
jahid45780 <jahidhossain88899@gmail.com>
2025-03-20 07:42:07 +0600

    add the plm6

--- PLM6/plm6.js
+++ PLM6/plm6.js
@@ -1,1 +1,15 @@
-const dulikentNumber=[1,2,3,4,5,6,77,1,2,3,5,8,89,2]
+// const numbers=[1,2,3,4,5,6,77,1,2,3,5,8,89,2]
+// const duplicates =numbers.filter(function(value, index, array){
+//     return array.indexOf(value)  !== index
+// }) 
+
+// console.log(duplicates);
+
+// unik number 
+const  numbers = [1,2,3,4,5,55,67,7,1,2,3,]
+const unik = numbers.filter(function(value, index,array){
+    return   array.indexOf(value) === index;
+})
+
+console.log(unik);
+
