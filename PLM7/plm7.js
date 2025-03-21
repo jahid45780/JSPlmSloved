6a501161cb3713cacfa61eaf1d12032693f958ad
jahid45780 <jahidhossain88899@gmail.com>
2025-03-21 21:53:05 +0600

    add the sloved

--- PLM7/plm7.js
+++ PLM7/plm7.js
@@ -1,2 +1,10 @@
 const sentence = "Jahid is a passionate developer who loves coding and problem-solving. Jahid enjoys working with React, Next.js, and Tailwind CSS. Jahid believes in continuous learning and self-improvement. Jahid also has a keen interest in UI/UX design. Jahid spends his free time exploring new technologies. Jahid’s dedication to excellence sets him apart."
 
+const matches = sentence.match(/jahid/gi)
+
+// console.log(matches);
+const ocurances = matches ? matches.length : 0;
+// console.log(ocurances);
+
+let position = sentence.search(/jahid/i)
+position = position =>0 ? position : "not found data"
